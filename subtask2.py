911de4140f1c4e89333248e0218bb177bf2aa60d
Lucia <s2310769023@fhooe.at>
2023-12-20 20:51:36 +0100

    added subtask1.py

---
+++ subtask2.py
@@ -0,0 +1,22 @@
+def SumMaxMinAvgCount(): 
+    n = 0 
+    s = 0 
+    m = float('inf')
+    x = int(input("Enter a natural number: "))
+    while x != -1: 
+        n = n+1 
+        s = s+x 
+        m = min(m,x)
+        x = int(input("Enter -1 to terminate code or a natural number: "))
+    if n == 0: 
+        print ("Input is not given")
+        
+    else: 
+        a = s/n
+        print ("This is the count = ", n) 
+        print ("This is the sum = " , s )
+        print ("This is the minimun = ", m)
+        print ("This is the mean = ", a)
+        
+SumMaxMinAvgCount()
+
